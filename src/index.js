a7d363bcd93787547810ae05561859bea5abba35
Pietro Rhyan <pietroqjg@gmail.com>
2022-09-19 11:12:55 -0300

    Completing the challenge

--- src/index.js
+++ src/index.js
@@ -23,15 +23,54 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+ 
+  if(user.todos.length >= 10 && user.pro === false) {
+    return response.status(403).json({error: "User exceeded the amount of todos in the Free Plan!"})
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const validateUuid = validate(id)
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({error: "User not found!"})
+  }
+
+  if(!validateUuid) {
+    return response.status(400).json({error: "Invalid UUID"})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(404).json({error: "Todo id not found!"})
+  }
+
+  request.user = user
+  request.todo = todo
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const userById = users.find(user => user.id === id)
+
+  if(!userById) {
+    return response.status(404).json({error: "User not found!"})
+  }
+
+  request.user = userById
+  return next()
 }
 
 app.post('/users', (request, response) => {
